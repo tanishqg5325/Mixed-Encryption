da8d92267532c72bff6d8010c421ac9b9bc31bc1
Tanishq Gupta <tanishqg2406@gmail.com>
2020-09-19 15:09:31 +0530

    added header guards

--- prime.h
+++ prime.h
@@ -1,6 +1,11 @@
 /*/------------------Functions to generate Primes and Strong Primes-------------------/*/
 
+#ifndef PRIME_H
+#define PRIME_H
+
 #include <gmpxx.h>
 
 mpz_class generate_prime (int);
 mpz_class generate_strong_prime (int);
+
+#endif
--- vignere.h
+++ vignere.h
@@ -1,7 +1,13 @@
 /*/-------------------Encryption and Decryption using Vignere Cipher------------------/*/
 
+
+#ifndef VIGNERE_H
+#define VIGNERE_H
+
 #include <string>
 using std::string;
 
 string encrypt_vignere (string, string);
 string decrypt_vignere(string, string);
+
+#endif
